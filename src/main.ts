87a209fc20d02b18b79a22636363d0ba6dfeb34f
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-04-20 17:43:06 +0700

    docs: initialize swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('TOPCV')
+    .setDescription('TRANG WEB TUYEN DUNG VIEC LAM')
+    .setDescription('1.0')
+    .build() 
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs',app, documentFactory);
+
   const PORT = configService.get('PORT')
   await app.listen(PORT);
 }
