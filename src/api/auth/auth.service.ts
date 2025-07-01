83af162858da92506f8e7d24760375b3038e4ed4
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-07-01 16:57:45 +0700

    chore(user): Create session

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { UsersService } from '../users/users.service';
+import { Status } from 'src/constants/app.constant';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private usersService: UsersService) {}
+
+    async signIn(loginDto: LoginDto) {
+        const {email, password} = loginDto;
+        const user = await this.usersService.findUserByEmail(email);
+        if(!user || !await user.isMatchPassword(password)) {
+            throw new UnauthorizedException("Invalid email or password");
+        }
+        if(user.status === Status.INACTIVE) {
+            throw new UnauthorizedException("User status is inactive")
+        }
+        
+
+    }
+}
--- src/api/users/users.service.ts
+++ src/api/users/users.service.ts
@@ -9,14 +9,31 @@ import { plainToInstance } from 'class-transformer';
 import { OffsetPaginatedDto } from 'src/common/dto/offset-pagination/offset-paginated.dto';
 import { QueryUserDto } from './dto/query-user.dto';
 import { OffsetPaginationDto } from 'src/common/dto/offset-pagination/offset-pagination.dto';
-
+import { CreateSessionDto } from './dto/create-session.dto';
+import { SessionEntity } from './entities/session.entity';
+import { ResponseSessionDto } from './dto/response-session.dto';
+import { ConfigService } from '@nestjs/config';
+import * as ms from "ms"
 @Injectable()
 export class UsersService {
 
-  constructor(@InjectRepository(UserEntity) private usersRepository: Repository<UserEntity> ) {
+  constructor(
+    @InjectRepository(UserEntity) private usersRepository: Repository<UserEntity>,
+    @InjectRepository(SessionEntity) private sessionsRepository: Repository<SessionEntity>,
+    private configService: ConfigService
+  ) {
 
   }
   
+  async createSession(createSessionDto: CreateSessionDto) :Promise<ResponseSessionDto> {
+    const session = await this.sessionsRepository.create({
+      ...createSessionDto,
+      expiresAt: new Date(Date.now() + ms(this.configService.get<ms.StringValue>('JWT_ACCESS_EXPIRES')))
+    }).save();
+    return plainToInstance(ResponseSessionDto, session);
+    
+  }
+
   async create(createUserDto: CreateUserDto) :Promise<ResponseUserDto> {
     const user = this.usersRepository.create(createUserDto);
     await user.save();
