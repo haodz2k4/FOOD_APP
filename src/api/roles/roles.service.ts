f46f995410b35fcadada8b5b964e6fbc3b6cefb0
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-07-02 10:44:36 +0700

    chore(role): find role by name

--- src/api/roles/roles.service.ts
+++ src/api/roles/roles.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RoleEntity } from './entities/role.entity';
+import { Repository } from 'typeorm';
+import { Role } from 'src/constants/role.constant';
 
 @Injectable()
 export class RolesService {
+
+  constructor(@InjectRepository(RoleEntity) private rolesRepository: Repository<RoleEntity>) {}
+
+  findRoleByName(name: Role) {
+    return this.rolesRepository.findOneBy({title: name})
+  }
+
   create(createRoleDto: CreateRoleDto) {
     return 'This action adds a new role';
   }
