4b3915781960c5fea95feb5b4a003bad28f15a28
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-07-09 19:10:23 +0700

    chore: fix version npm

--- src/api/products/entities/product.entity.ts
+++ src/api/products/entities/product.entity.ts
@@ -1,1 +1,28 @@
-export class Product {}
+import { AbstractEntity } from "src/database/entities/abstracts.entity";
+import { Column, DeleteDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity('products')
+export class ProductEntity extends AbstractEntity {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    title: string;
+
+    @Column({nullable: true})
+    description: string;
+
+    @Column({nullable: true})
+    thumbnail: string;
+
+    @Column()
+    price: number;
+
+    @Column()
+    discountPercentage: number;
+
+    @DeleteDateColumn()
+    deletedAt: Date;
+}
