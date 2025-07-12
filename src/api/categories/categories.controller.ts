abc45e634fac20bbb2c8de9cc5de09907b4327b1
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-07-12 15:09:37 +0700

    feat(categories): get many

--- src/api/categories/categories.controller.ts
+++ src/api/categories/categories.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { ResponseCategoryDto } from './dto/response-category.dto';
 import { Message } from 'src/decorators/message.decorator';
+import { QueryCategoryDto } from './dto/query-category.dto';
+import { OffsetPaginatedDto } from 'src/common/dto/offset-pagination/offset-paginated.dto';
 
 @Controller('categories')
 export class CategoriesController {
@@ -16,8 +18,9 @@ export class CategoriesController {
   }
 
   @Get()
-  findAll() {
-    return this.categoriesService.findAll();
+  @Message("Get categories")
+  findAll(@Query() queryCategoryDto: QueryCategoryDto) :Promise<OffsetPaginatedDto<ResponseCategoryDto>> {
+    return this.categoriesService.findAll(queryCategoryDto);
   }
 
   @Get(':id')
--- src/api/categories/categories.service.ts
+++ src/api/categories/categories.service.ts
@@ -6,6 +6,9 @@ import { CategoryEntity } from './entities/category.entity';
 import { Repository } from 'typeorm';
 import { ResponseCategoryDto } from './dto/response-category.dto';
 import { plainToInstance } from 'class-transformer';
+import { QueryCategoryDto } from './dto/query-category.dto';
+import { OffsetPaginationDto } from 'src/common/dto/offset-pagination/offset-pagination.dto';
+import { OffsetPaginatedDto } from 'src/common/dto/offset-pagination/offset-paginated.dto';
 
 @Injectable()
 export class CategoriesService {
@@ -18,8 +21,23 @@ export class CategoriesService {
     return plainToInstance(ResponseCategoryDto, category);
   }
 
-  findAll() {
-    return `This action returns all categories`;
+  async findAll(queryCategoryDto: QueryCategoryDto) :Promise<OffsetPaginatedDto<ResponseCategoryDto>> {
+    const {keyword, status, limit, sortBy, sortOrder} = queryCategoryDto;
+    const queryBuilder = this.categoriesRepository
+    .createQueryBuilder('categories')
+    .limit(limit)
+    .offset(queryCategoryDto.offset())
+    .orderBy(`categories.${sortBy}`, sortOrder)
+    if(keyword) {
+      queryBuilder.andWhere("categories.title LIKE :keyword",{keyword: `%${keyword}%`})
+    }
+    if(status) {
+      queryBuilder.andWhere("categories.status LIKE :status",{status});
+    }
+    const [categories, count] = await queryBuilder.getManyAndCount();
+    const offsetPagination = new OffsetPaginationDto(count,queryCategoryDto);
+
+    return new OffsetPaginatedDto(plainToInstance(ResponseCategoryDto, categories), offsetPagination)
   }
 
   findOne(id: number) {
