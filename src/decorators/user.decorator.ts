2023486cdffd2ba5b8f72a5c090040ebb4f280d4
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-07-18 15:03:25 +0700

    chore: user decorator

---
+++ src/decorators/user.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+
+
+
+export const User = createParamDecorator(
+    (field: string | undefined, ctx: ExecutionContext) => {
+        const req = ctx.switchToHttp().getRequest();
+        return field ? req.user[field] : req.user
+    }
+)
